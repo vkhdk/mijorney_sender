adc5ee1f5c56804d15e5f759644031f2970a1f19
Vova <vkhdk@yandex.ru>
2023-01-27 12:40:51 +0300

    just draft to send message

---
+++ authorization_secrets.py
@@ -0,0 +1,7 @@
+# authorization data
+
+# chanel url from test message
+chanel_url = 'https://discord.com/api/v9/channels/1067848867523068041/messages'
+
+# user id from test message
+user_id = 'MTA2NzgzOTcxMDI0NTk1MzU1OA.GdbGex.a3rs8VdUAQ9ep9-ocy-YB6lxLGx4j1xmFgjRDs'
--- discord_sender.py
+++ discord_sender.py
@@ -3,6 +3,28 @@ import requests
 
 # import specific libraries for discord
 
+# importing internal modules
+import authorization_secrets
+
+def send_message_to_chanel(chanel_url, user_id, content_text):
+    auth = {
+    'authorization': user_id
+    }
+    
+    msg = {
+    'content': content_text
+    }
+
+    requests.post(chanel_url, headers = auth, data = msg)
 
 if __name__ == '__main__':
-    print('Get ready mijorney!')
+    chanel_url = authorization_secrets.chanel_url
+    user_id = authorization_secrets.user_id
+
+    print('Hello there!\nTo exit press press Enter.\nTo send message enter text:', end='')
+    content_text = input()
+    
+    if content_text:
+        send_message_to_chanel(chanel_url, user_id, content_text)
+    else:
+        print('See you next time!')
